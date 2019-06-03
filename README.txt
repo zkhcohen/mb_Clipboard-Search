09b824d3e5bbf8cb582bf6917c44c2e9e9710437
zkhcohen <zacharykhcohen@gmail.com>
2019-06-03 17:34:51 -0400

    First.

---
+++ README.txt
@@ -0,0 +1,12 @@
+mb_Clipboard-Search Plugin
+
+Description:
+
+Allows you to assign a hotkey to search your Library using the text in your Windows Clipboard.
+
+
+Instructions: 
+
+1. Drag mb_Clipboard-Search.dll from the Plugins folder to your MusicBee Plugins folder, typically located at: C:\Program Files (x86)\MusicBee\Plugins.
+2. Open MusicBee and go to Edit > Edit Preferences > Plugins and enable mb_Clipboard-Search.
+3. Go to the Hotkeys section of the Preferences Pane and scroll down to "General: Search for Clipboard Text". Assign a hotkey.
---
+++ Source Files/ClipboardSearch.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace MusicBeePlugin
+{
+    public partial class Plugin
+    {
+        private MusicBeeApiInterface mbApiInterface;
+        private PluginInfo about = new PluginInfo();
+
+        public PluginInfo Initialise(IntPtr apiInterfacePtr)
+        {
+            mbApiInterface = new MusicBeeApiInterface();
+            mbApiInterface.Initialise(apiInterfacePtr);
+            about.PluginInfoVersion = PluginInfoVersion;
+            about.Name = "mb_Clipboard-Search";
+            about.Description = "Allows you to assign a hotkey to search for Clipboard text.";
+            about.Author = "Zachary Cohen";
+            about.TargetApplication = "";   // current only applies to artwork, lyrics or instant messenger name that appears in the provider drop down selector or target Instant Messenger
+            about.Type = PluginType.General;
+            about.VersionMajor = 1;  // your plugin version
+            about.VersionMinor = 0;
+            about.Revision = 0;
+            about.MinInterfaceVersion = MinInterfaceVersion;
+            about.MinApiRevision = MinApiRevision;
+            about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
+            about.ConfigurationPanelHeight = 0;   // height in pixels that musicbee should reserve in a panel for config settings. When set, a handle to an empty panel will be passed to the Configure function
+            return about;
+        }
+
+        public bool Configure(IntPtr panelHandle)
+        {
+            return false;
+        }
+       
+        // called by MusicBee when the user clicks Apply or Save in the MusicBee Preferences screen.
+        // its up to you to figure out whether anything has changed and needs updating
+        public void SaveSettings()
+        {
+        }
+
+        // MusicBee is closing the plugin (plugin is being disabled by user or MusicBee is shutting down)
+        public void Close(PluginCloseReason reason)
+        {
+        }
+
+        // uninstall this plugin - clean up any persisted files
+        public void Uninstall()
+        {
+        }
+
+        // receive event notifications from MusicBee
+        // you need to set about.ReceiveNotificationFlags = PlayerEvents to receive all notifications, and not just the startup event
+        public void ReceiveNotification(string sourceFileUrl, NotificationType type)
+        {
+            switch (type)
+            {
+                case NotificationType.PluginStartup:
+                    
+                    mbApiInterface.MB_RegisterCommand("General: Search for Clipboard Text", searchLibrary);
+
+                    break;
+            }
+        }
+
+       
+        private void searchLibrary(object sender, System.EventArgs e)
+        {
+
+            if(Clipboard.ContainsText())
+            {
+
+                string searchTerm = Clipboard.GetText();
+
+                mbApiInterface.MB_OpenFilterInTab(MetaDataType.TrackTitle, ComparisonType.Contains, searchTerm, MetaDataType.TrackTitle, ComparisonType.Contains, searchTerm);
+                
+
+                
+            }
+            else
+            {
+
+                MessageBox.Show("No text found in Clipboard!");
+
+            }
+            
+        }
+
+
+
+
+   }
+}
